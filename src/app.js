b3c4411bccc51b729e2c44ce1d7f4dea6cc7b1d3
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2021-07-14 18:34:20 -0400

    add modality and specialty search

--- src/app.js
+++ src/app.js
@@ -1,33 +1,37 @@
-import express from 'express';
-import path from 'path';
-import ejs from 'ejs';
-import bodyParser from 'body-parser';
+import express from "express";
+import path from "path";
+import ejs from "ejs";
+import bodyParser from "body-parser";
 const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: false }))
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(__dirname + '/views'))
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(__dirname + "/views"));
 
-app.use('/public', express.static(__dirname + '/public'))
+app.use("/public", express.static(__dirname + "/public"));
 
-app.get('/', (req, res) => {
-    return res.render('home')
+app.get("/", (req, res) => {
+  return res.render("home");
 });
 
-app.get('/search', (req, res) => {
-    return res.render('search')
+app.get("/search", (req, res) => {
+  return res.render("search");
 });
 
-// app.get('/update/:id', (req, res) => {
-//     return res.render('updateMovie')
-// });
-// app.get('/allMovies/:id', (req, res) => {
-//     return res.render('allMovies')
-// });
+app.get("/search?state=", (req, res) => {
+  return res.render("search");
+});
 
-app.listen(port, () => console.log('server is running'));
+app.get("/search?specialty=", (req, res) => {
+  return res.render("search");
+});
+app.get("/search?modality=", (req, res) => {
+  return res.render("search");
+});
+
+app.listen(port, () => console.log("server is running"));
